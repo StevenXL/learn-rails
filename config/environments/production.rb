157bc9262995856e027f63c85115f015071e731f
StevenXL <leiva.steven@gmail.com>
2015-03-03 15:14:48 -0500

    Corrected config/environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Rails.application.configure do
 
   # enable e-mail in production
   config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
-  config.action_mailer.deliver_method = :smtp
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.smtp_settings = {
